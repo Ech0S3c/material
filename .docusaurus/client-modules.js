95a334c07f8250b7fa5795183329bd7ebe56f6b4
JvWandermurem <joao.oliveira@sou.inteli.edu.br>
2025-06-11 15:08:09 -0300

    Deploy website - based on 5bced78cf216f44550b3b1e4e7432771b1f85dad

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/inteli/Documentos/material-echosec/material/node_modules/infima/dist/css/default/default.css"),
-  require("/home/inteli/Documentos/material-echosec/material/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/inteli/Documentos/material-echosec/material/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/inteli/Documentos/material-echosec/material/src/css/custom.css"),
-];
